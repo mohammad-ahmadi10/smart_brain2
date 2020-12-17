c00a317a648f006a0ef669e32d9e55c4d60639b8
Mohammad Ahmadi <52349443+Momo10-barca@users.noreply.github.com>
2020-12-17 23:27:01 +0100

    another change

--- src/routes/forget.js
+++ src/routes/forget.js
@@ -74,7 +74,7 @@ const checkTheUser = async (res ,email) =>{
                     <br/>please click on this Link in order to reset your password
                     <a href="https://cryptic-bastion-84659.herokuapp.com/passwordReset/${token}"><strong>Link</strong></a>
                     if you are not ${username} please ignore this Email`
-        }
+           } 
  
         transprot.sendMail(mailOptions, (err, info) =>{
             if(err){
