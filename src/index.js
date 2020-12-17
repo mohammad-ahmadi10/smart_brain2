25d556370abf766d789375cdafc2f6e97d11d08f
Mohammad Ahmadi <52349443+Momo10-barca@users.noreply.github.com>
2020-12-17 23:09:59 +0100

    another change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-serviceWorker.register();
